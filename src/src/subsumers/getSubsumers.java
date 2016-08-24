cf1af4ed6a2fa0dd85a3386421d48a2979dcae72
prashanti <manda.prashanti@gmail.com>
2016-08-24 09:53:57 -0400

    minor formatting changes

--- src/src/subsumers/getSubsumers.java
+++ src/src/subsumers/getSubsumers.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.Set;
-
 import org.semanticweb.elk.owlapi.ElkReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.expression.ParserException;
@@ -17,17 +16,12 @@ import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-
 // Usage
 // javac -cp .:/Users/pmanda/Documents/JARs/org.semanticweb.owl.owlapi-4.1.jar:/Users/pmanda/Documents/JARs/elk-owlapi.jar:/Users/pmanda/Documents/JARs/log4j-1.2.17.jar ./subsumers/getSubsumers.java
-
 // java -cp .:/Users/pmanda/Documents/JARs/org.semanticweb.owl.owlapi-4.1.jar:/Users/pmanda/Documents/JARs/elk-owlapi.jar:/Users/pmanda/Documents/JARs/log4j-1.2.17.jar subsumers/getSubsumers ../../data/go-plus.owl ../../data/GO_Subsumers.tsv
 
-public class getSubsumers 
-{
-
-	public static void main(String[] args) throws IOException, OWLOntologyStorageException, OWLOntologyCreationException,  ParserException, ClassNotFoundException, SQLException 
-	{
+public class getSubsumers {
+	public static void main(String[] args) throws IOException, OWLOntologyStorageException, OWLOntologyCreationException,  ParserException, ClassNotFoundException, SQLException {
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 		File inputont = new File(args[0]);
 	    PrintWriter printWriter = new PrintWriter (args[1]);
@@ -41,15 +35,8 @@ public class getSubsumers
 	        	for(OWLAnnotationAssertionAxiom annotations:subsumer.getAnnotationAssertionAxioms(ontology)){
 	    		    if (annotations.getProperty().getIRI().getFragment().toString().equals("hasOBONamespace")){
 	                	if (annotations.getValue().toString().contains("cellular_component")){
-	                		printWriter.write((cls.toString()+"\t"+subsumer.toString()+"\n").replace("<http://purl.obolibrary.org/obo/", "").replace(">", ""));
-	                	}
-	                }
-	    		}
-	        }
-	    }
-		printWriter.close();	
-	}
-}
-
+	                		printWriter.write((cls.toString()+"\t"+subsumer.toString()+"\n").replace("<http://purl.obolibrary.org/obo/", "").replace(">", ""));}}}}}
+	     printWriter.close();}}	
+	
 
 
